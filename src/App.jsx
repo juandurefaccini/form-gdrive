b59f9b0b5a2919cb75931098f1056434ec803899
juandurefaccini <juanchidure@outlook.com>
2023-02-06 15:06:10 -0300

    Update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,25 @@ import Home from "./components/Home";
 import TopBar from "./components/TopBar";
 import UploadForm from "./components/UploadForm";
 import { AuthProvider } from "./context/authContext.jsx";
+import { SnackbarProvider } from "notistack";
 
 function App() {
   return (
-    <div className="h-screen bg-white flex flex-col">
-      <AuthProvider>
-        <div className="h-12 shrink-0">
-          <TopBar />
-        </div>
-        <div className="h-full w-full">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/subirArchivo" element={<UploadForm />} />
-          </Routes>
-        </div>
-      </AuthProvider>
-    </div>
+      <SnackbarProvider>
+        <AuthProvider>
+          <div className="h-screen bg-white flex flex-col">
+            <div className="h-12 shrink-0">
+              <TopBar />
+            </div>
+            <div className="h-full w-full">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/subirArchivo" element={<UploadForm />} />
+              </Routes>
+            </div>
+          </div>
+        </AuthProvider>
+      </SnackbarProvider>
   );
 }
 
